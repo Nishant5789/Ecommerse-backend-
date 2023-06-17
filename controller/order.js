275c2d1807c30b9308c343f388a8460479491aad
Nishant5789 <nishantbhandari218@gmail.com>
2023-06-17 22:22:09 +0530

    add validate params for admin apis

--- controller/order.js
+++ controller/order.js
@@ -10,7 +10,69 @@ module.exports.fetchOrderByUser = async (req, res)=>{
         return res.status(400).json(error);
     }
 }
+
+module.exports.updateOrder = async (req, res)=>{
+    const {id} = req.params;
+    try {
+        const docs = await Order.findByIdAndUpdate(id, req.body);
+        return res.status(201).json(docs);
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json(error);
+    }
+}
+
+module.exports.fetchAllOrder = async (req, res)=>{
+
+    http://localhost:8080/order?_sort=totalAmount&_order=asc&_admin=true
+    try {  
+        let query =  Order.find({deleted: {$ne: true}});
+        let totalOrdersQuery =  Order.find({deleted: {$ne: true}});
+
+        if(req.query._sort && req.query._order){
+            query = query.sort({[req.query._sort]: req.query._order});
+            totalOrdersQuery = totalOrdersQuery.sort({[req.query._sort]: req.query._order});
+        }
+
+        const totalDocs = await totalOrdersQuery.count().exec(); 
+        console.log(totalDocs);
+        res.set('X-Total-Count', totalDocs)
+
+        if(req.query._page && req.query._limit){
+            const page = req.query._page;
+            const pageSize = req.query._limit;
+            query = query.skip(pageSize*(page-1)).limit(pageSize);
+        }
+
+        const docs = await query.exec();
+        res.status(201).json(docs);
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json(error);
+    }
+}
+
+
 module.exports.createOrder = async (req, res)=>{
+
+    // {
+    //     "items": [
+    //       "648c599daf9199aa139b2863"
+    //     ],
+    //     "totalAmount": 700,
+    //     "totalItems": 1,
+    //     "user": "648d6af31a181df43848f424",
+    //     "payment": "CASH",
+    //     "status": "Delivered",
+    //     "selectedAddress": {
+    //       "name": "nishant",
+    //       "state": "gujarat",
+    //       "district": "killa pardi",
+    //       "house": "shivpoojan residency",
+    //       "houseno": "A-102"
+    //     }
+    //   }
+
     try {
         const order = new Order(req.body);
         await order.save()
--- routes/order.js
+++ routes/order.js
@@ -1,9 +1,11 @@
 const {  } = require('../controller/cart');
-const { createOrder, deleteOrder, fetchOrderByUser } = require('../controller/order');
+const { createOrder, deleteOrder, fetchOrderByUser, fetchAllOrder, updateOrder } = require('../controller/order');
 const router = require('express').Router();
 
 router.post('/', createOrder);
 router.delete('/:id', deleteOrder);
 router.get('/:id', fetchOrderByUser);
+router.get('/', fetchAllOrder);
+router.patch('/:id', updateOrder);
 
 module.exports = router;
