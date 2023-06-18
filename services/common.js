d0829fcaed7fc72af4bb73682e3e148f654cc0eb
Nishant5789 <nishantbhandari218@gmail.com>
2023-06-18 23:03:35 +0530

    feat : devlop  passport-jwt for send json token using cookie

--- services/common.js
+++ services/common.js
@@ -1,14 +1,11 @@
+const passport = require("passport");
+
 module.exports.sanitizeUser = (user) => {
     return { id: user.id, role: user.role };
-};
+  };
   
 module.exports.isAuth = (req, res, done) => {
-    if(req.user){
-        done();
-    }  
-    else{
-        res.send(401);
-    }
-};
+    return passport.authenticate('jwt');
+  };
   
   
